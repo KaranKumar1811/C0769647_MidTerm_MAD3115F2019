5e3db7abcbc9df80f3c8b82ad4fb6f1a135b23ad
Karan <kumarkay.ca@gmail.com>
2019-11-01 13:44:47 -0400

    Adding Buttons and text field

---
+++ C0769647_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  C0769647_MidTerm_MAD3115F2019
+//
+//  Created by MacStudent on 2019-11-01.
+//  Copyright © 2019 Karan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBAction func loginBtn(_ sender: UIButton) {
+       
+    }
+    
+    @IBOutlet weak var passwordTxtField: UITextField!
+    
+    @IBOutlet weak var usernameTxtField: UITextField!
+    
+    @IBAction func rememberMeSwitch(_ sender: UISwitch) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+}
+
+
