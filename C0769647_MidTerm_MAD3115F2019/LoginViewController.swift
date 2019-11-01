a3f28a297e533cbddaf3c186e0b306f753c32a59
Karan <Kumarkay.ca@gmail.com>
2019-11-01 15:04:13 -0400

    Login

--- C0769647_MidTerm_MAD3115F2019/LoginViewController.swift
+++ C0769647_MidTerm_MAD3115F2019/LoginViewController.swift
@@ -11,9 +11,32 @@ import UIKit
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var rememberSwitchBtn: UISwitch!
     @IBAction func loginBtn(_ sender: UIButton) {
-        let clvc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "CustomerListVC")
-        self.navigationController?.pushViewController(clvc, animated: true)
+        
+        if readInfoPlist()
+        {
+            if self.rememberSwitchBtn.isOn{
+                self.userDefault.set(usernameTxtField.text, forKey: "userName")
+                self.userDefault.set(passwordTxtField.text, forKey: "userPassword")
+            }else{
+                self.userDefault.removeObject(forKey: "userName")
+                self.userDefault.removeObject(forKey: "userPassword")
+            }
+            
+            let clvc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "CustomerListVC")
+            self.navigationController?.pushViewController(clvc, animated: true)
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
     @IBOutlet weak var passwordTxtField: UITextField!
@@ -22,16 +45,24 @@ class LoginViewController: UIViewController {
     
     var userDefault : UserDefaults!
     
-    @IBAction func rememberMeSwitch(_ sender: UISwitch) {
-    }
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let getdata =  Singleton.getInstance()
         getdata.createCustomer()
         userDefault = UserDefaults.standard
+        if let userName = userDefault.value(forKey: "userName"){
+            usernameTxtField.text = userName as? String
+        }
+        
+        if let userPassword = userDefault.value(forKey: "userPassword"){
+            passwordTxtField.text = userPassword as? String
+        }
         
     }
+    
+    
     
     func readInfoPlist() -> Bool{
         if let bundlePath = Bundle.main.path(forResource: "Users", ofType: "plist") {
