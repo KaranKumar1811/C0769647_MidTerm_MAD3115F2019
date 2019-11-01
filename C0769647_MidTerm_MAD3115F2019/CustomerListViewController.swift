ac1c9dd73865448247aa078ea1467708140ef3e2
Karan <kumarkay.ca@gmail.com>
2019-11-01 13:47:16 -0400

    Customer List VC

---
+++ C0769647_MidTerm_MAD3115F2019/CustomerListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CustomerListViewController.swift
+//  C0769647_MidTerm_MAD3115F2019
+//
+//  Created by MacStudent on 2019-11-01.
+//  Copyright © 2019 Karan. All rights reserved.
+//
+
+import UIKit
+
+class CustomerListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+  
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
