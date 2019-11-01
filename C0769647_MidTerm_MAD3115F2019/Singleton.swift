fae10cfc1a3d6f8539b42e619f8be89cd7bd7763
Karan <Kumarkay.ca@gmail.com>
2019-11-01 13:56:01 -0400

    Singleton Class

---
+++ C0769647_MidTerm_MAD3115F2019/Singleton.swift
@@ -0,0 +1,13 @@
+//
+//  Singleton.swift
+//  C0769647_MidTerm_MAD3115F2019
+//
+//  Created by MacStudent on 2019-11-01.
+//  Copyright © 2019 Karan. All rights reserved.
+//
+
+import Foundation
+class Singleton : NSObject
+{
+    
+}
